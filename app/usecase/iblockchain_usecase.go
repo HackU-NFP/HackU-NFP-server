20701db798683f2a3ea3b2f2bb191207bbc67669
ga-tcho <nagata.twi@gmail.com>
2022-02-23 06:54:06 +0900

    add: NFT取得api

--- app/usecase/iblockchain_usecase.go
+++ app/usecase/iblockchain_usecase.go
@@ -5,4 +5,6 @@ type IBlockchainUseCase interface {
 	CreateNonFungible(userID, contractID, name, meta string) (*TransactionAccepted, error)                 //発行
 	MintNonFungible(userID, contractID, tokenType string, name, meta string) (*TransactionAccepted, error) //鋳造
 	GetTransaction(txHash string) (*Transaction, error)
+	GetNonFungibles(contractID, orderBy, limit, page string) ([]*NonFungible, error)
+	GetNonFungibleInfo(contractID, tokenType string) (*NonFungibleInfo, error)
 }
